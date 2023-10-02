4257b25f09879336e8c133a11dddfb719b1db356
GavishkaChamindu <gavichamindu@gmail.com>
2023-10-02 11:29:19 +0530

    create post table

---
+++ database/migrations/2023_10_02_052025_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author_name');
+            $table->string('content');
+            $table->string('image');
+            $table->unsignedBigInteger(' user_id');
+            $table->primary(['id', 'user_id']);
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('posts')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
