5fa0330d685b9bee21adb86303bead7922b1e710
GavishkaChamindu <gavichamindu@gmail.com>
2023-10-02 12:54:00 +0530

    create data insert

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+     //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $req)
+    {
+
+
+$validateData = $req->validate([
+
+    'title' => 'required',
+    'author_name' => 'required',
+    'content' => 'required',
+    'image' => 'required',
+   'user_id' => 'required'
+
+]);
+
+$post = new Post;
+
+$post->title = $validateData['title'];
+$post->author_name = $validateData['author_name'];
+$post->content = $validateData['content'];
+$post->image = $validateData['image'];
+$post->user_id = $validateData['user_id'];
+
+$post->save();
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
